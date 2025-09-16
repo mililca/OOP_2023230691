b4f7be257a0175f9aef7343703465bd0662bc8ff
mililca <141839434+mililca@users.noreply.github.com>
2025-09-16 08:51:31 +0200

    Create CardClass.java

---
+++ CardClass.java
@@ -0,0 +1,8 @@
+// Public enum type for Card Classes
+// Each class is a different memory card category
+
+public enum CardClass {
+    CLASS_4,  // Slow
+    CLASS_6,  // Medium
+    CLASS_10  // Best
+}
