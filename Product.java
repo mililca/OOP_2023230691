dbe27f03d5f04d7d2f64d44a86d60d0e074ac9df
mililca <141839434+mililca@users.noreply.github.com>
2025-09-16 09:18:54 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,20 +1,17 @@
-// Stores data about the product identifier in the inventory of the store, and price
+// Stores data about the product identifier in the inventory of the store and price
 public abstract class Product {
 
     private final String productID; // Unique identifier for each product
     private double price; // The price of the product
-    private String manufacturer; // The manufacturer of the product
 
-    public Product(String productID, double price, String manufacturer) {
+    public Product(String productID, double price) {
         this.productID = productID; // Assigning a unique product ID
         this.price = price; // Assigning a price to the product
-        this.manufacturer = manufacturer; // Assigning the manufacturer to the product
     }
 
-    // Getter methods to get the product ID, price, and manufacturer
+    // Getter methods to get the product ID and price
     public String getProductID() { return productID; }
     public double getPrice() { return price; }
-    public String getManufacturer() { return manufacturer; }
 
     // Abstract method that will be implemented in the extended classes
     public abstract double getSuitability();
